42d011123c0fd297484bf09e37f2a0657ed3296e
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-11-28 11:20:56 +0000

    add template form

--- planetbook/settings.py
+++ planetbook/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'allauth',
+    'allauth.account',
+    'library',
+    'crispy_forms',
+    'checkout',
 ]
 
 MIDDLEWARE = [
